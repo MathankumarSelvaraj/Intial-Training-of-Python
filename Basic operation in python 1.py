7e865b0b885ef9ca46aa9fe905a13bea519568f4
Mathankumar <46513026+MathankumarSelvaraj@users.noreply.github.com>
2019-02-16 19:53:30 +0530

    Add files via upload

---
+++ Basic operation in python 1.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print('Hello World')
+
+
+# In[5]:
+
+
+age = 10
+Name = 'Mathan'
+type(age)
+type(Name)
+
+
+# In[7]:
+
+
+print("Name =%s and Age = %d" %(Name,age))
+#this is for some output funtion for python
+
+
+# In[8]:
+
+
+x=[1,2,3,4,5]
+type(x)
+
+
+# In[9]:
+
+
+min(x)
+max(x)
+
+
+# In[10]:
+
+
+sum(x)
+
+
+# In[11]:
+
+
+a=("Mathan")#this is for string opreation
+len(a)
+
+
+# In[13]:
+
+
+b="kumar"#this is string concatination
+print(a+b)
+
+
+# In[15]:
+
+
+#here some string funtions let's see
+s1='this is single quotes string'
+s2="this is for double quotes string "
+s3='''this is for triple quotes string'''
+print(s1+s2)
+
+
+# In[16]:
+
+
+s1[0:3]# this is geting particular charactors fron the sring its goes from right to left
+
+
+# In[17]:
+
+
+s1[0:10:2]#this is go by step of 3
+
+
+# In[26]:
+
+
+s1[-8:-1]#this is getting character from left to right
+
+
+# In[20]:
+
+
+len(s1)
+
+
+# In[31]:
+
+
+s1[len(s1)-10:len(s1)-5]#this is give geting particular chunk in the larger sring
+
+
+# In[32]:
+
+
+q='1'#this type is sring not a integer because that is in single quotes whenever you type anthing in quotes its consider as sring
+w='2'
+print(q+w)
+
+
+# In[33]:
+
+
+int(q)+int(w)#here I changed the sting as a interger also we can add it but one thing is some sring can be change as string not for all like int("python")
+
+
+# In[36]:
+
+
+p=[5,6,7,8]
+p[1]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
